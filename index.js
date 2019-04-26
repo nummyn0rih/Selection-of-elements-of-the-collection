ce484af43cf9008bb35b0aef54ad4323625fb5a3
Ivan Malshchukov <nummyn0rih@gmail.com>
2019-04-26 18:42:00 +0300

    added f select функция принимает аргументы, на основании которых производится отбор свойств в объектах коллекции

--- index.js
+++ index.js
@@ -10,8 +10,27 @@ function query(collection) {
 /**
  * @params {String[]}
  */
-function select() {
+function select() { // функция принимает аргументы, на основании которых 
+                    // производится отбор свойств в объектах коллекции 
+    console.log(arguments);      // объект с аргументами
+    console.log(copyCollection); // коллекция объектов friends
+    let objectKeys = [];         // name,gender,email,favoriteFruit
 
+    for (let i = 0; i < copyCollection.length; i++) { // проходим по каждому объекту коллекции
+        objectKeys = Object.keys(copyCollection[i]);  // в переменную objectKeys записывается 
+                                                      // массив со всеми свойствами итерируемого 
+                                                      // объекта коллекции
+        for (keyColl in copyCollection[i]) {          // каждое свойство объекта проверяется на
+            outer: for (keyArg in arguments) {        // наличие в списке аргументов ф-ции
+                if (keyColl === arguments[keyArg]) {  // если свойство есть в списке аргументов,
+                    break outer;                      // то завершаем внутренний цикл и проверяем
+                                                      // следующее свойство объекта
+                } else if (keyArg == arguments.length - 1) { // если свойства нет в списке 
+                    delete copyCollection[i][keyColl];       // аргументов, то текущее свойство
+                }                                            // удаляется из объекта
+            }
+        }
+    }
 }
 
 /**
